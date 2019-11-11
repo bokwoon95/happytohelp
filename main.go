222e2cd606f0e1b8cbf03cf42e4421f4854a1634
bokwoon <bokwoon.c@gmail.com>
2019-11-12 01:52:49 +0800

    scaffolded basic pages

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -23,9 +24,29 @@ func category(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "category.html")
 }
 
+func disclosure(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	switch r.Method {
+	case "GET":
+		t, err := template.ParseFiles("disclosure.html")
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		err = t.Execute(w, nil)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+	case "POST":
+		w.Write([]byte("hey ho"))
+	}
+}
+
 func main() {
 	http.HandleFunc("/", landing)
 	http.HandleFunc("/category", category)
+	http.HandleFunc("/disclosure", disclosure)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
